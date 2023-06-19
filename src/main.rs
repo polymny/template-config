d88e1760eaf3b852c2a7103d4524bb92c9f57cb4
Thomas Forgione <thomas@forgione.fr>
2023-06-19 14:54:49 +0200

    Cleaner error messages

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use std::collections::BTreeMap;
+use std::error::Error;
 use std::{env, fs};
 
 use tera::{Context, Tera};
 
 fn main() {
-    let template_file = env::args().skip(1).next().unwrap();
-    let template_content = fs::read_to_string(&template_file).unwrap();
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        if let Some(source) = e.source() {
+            eprintln!("{}", source);
+        }
+    }
+}
 
-    let context = Context::from_serialize(env::vars().collect::<BTreeMap<_, _>>()).unwrap();
-    let output = Tera::one_off(&template_content, &context, false).unwrap();
+fn run() -> Result<(), Box<dyn Error>> {
+    let template_file = env::args().skip(1).next().unwrap();
+    let template_content = fs::read_to_string(&template_file)?;
+
+    let context = Context::from_serialize(env::vars().collect::<BTreeMap<_, _>>())?;
+    let output = Tera::one_off(&template_content, &context, false)?;
 
     println!("{}", output);
+
+    Ok(())
 }
