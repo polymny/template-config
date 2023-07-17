675ecea16ebf21badb26d24930bac3f878619169
Thomas Forgione <thomas@forgione.fr>
2023-07-17 16:19:54 +0200

    Adds support for dotenv files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use std::collections::BTreeMap;
 use std::error::Error;
 use std::process::exit;
-use std::{env, fs};
+use std::{env, fmt, fs};
 
 use tera::{Context, Tera};
+
+#[derive(Debug)]
+struct CustomError(String);
+
+impl fmt::Display for CustomError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Error for CustomError {}
 
 fn main() {
     if let Err(e) = run() {
@@ -16,8 +27,23 @@ fn main() {
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
-    let template_file = env::args().skip(1).next().unwrap();
+    let args = env::args().collect::<Vec<_>>();
+
+    let template_file = match args.get(1) {
+        Some(f) => f,
+        _ => {
+            return Err(Box::new(CustomError(
+                "this program expects a template file as argument".into(),
+            )))
+        }
+    };
+
     let template_content = fs::read_to_string(&template_file)?;
+
+    if let Some(env_file) = args.get(2) {
+        dotenvy::from_filename(env_file)?;
+    }
+
     let env = env::vars().collect::<BTreeMap<_, _>>();
     let mut context = BTreeMap::new();
     context.insert("env", env);
